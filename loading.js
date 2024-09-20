1b838eaebaf01650e2e219eac26039b22a215d28
Deviantejsh <158575481+Deviantejsh@users.noreply.github.com>
2024-09-20 21:12:32 +0200

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   setTimeout(function () {
     document.getElementById('loading-screen').style.display = 'none';
+    document.getElementById('offline-points-display').classList.remove('hidden'); // Убираем класс hidden
+    document.getElementById('energy-display').classList.remove('hidden'); // Убираем класс hidden
   }, 7000); // 7 секунд
 });
